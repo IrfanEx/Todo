f40e37a38b11e3e0e94faa9f014f05f0c5f52478
IrfanEx <55917030+IrfanEx@users.noreply.github.com>
2021-07-03 19:33:44 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:todo/screens/todo_list_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/Screens/Welcome/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'HomeScreen/provider/todos.dart';
+import 'constants.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static final String title = 'Todo App With Firebase';
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => TodosProvider(),
+    child: MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Todo list',
+      title: title,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primaryColor: kPrimaryColor,
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: Color(0xFFf6f5ee),
       ),
-      home: TodoListScreeen(),
-    );
-  }
+      home: WelcomeScreen(),
+    ),
+  );
 }
---
+++ lib/screens/Signup/components/body.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/Login/login_screen.dart';
+import 'package:todo/Screens/Signup/components/background.dart';
+import 'package:todo/components/already_have_an_account_acheck.dart';
+import 'package:todo/components/rounded_button.dart';
+import 'package:todo/components/rounded_input_field.dart';
+import 'package:todo/components/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Body extends StatelessWidget {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/signup.svg",
+              height: size.height * 0.35,
+            ),
+            RoundedInputField(
+              hintText: "Your Name",
+              onChanged: (value) {},
+            ),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {
+                email = value;
+              },
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () async {
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if (newUser != null) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
